5a7cd40d6e51362820bb8cd25e8c2281c6dab19b
aingbager <aingbager55@gmail.com>
2023-10-06 08:48:29 +0700

    add fn kali

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_input1 = String::new();
+    print!("masukan angka: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input1).unwrap();
+
+    let mut user_input2 = String::new();
+    print!("masukan angka: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input2).unwrap();
+
+    let user_input1: i32 = user_input1.trim().parse().expect("inputkan angka");
+    let user_input2: i32 = user_input2.trim().parse().expect("inputkan angka");
+
+    let mut pilihan = String::new();
+    print!("pilih operasi [*/+-]: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut pilihan).unwrap();
+
+    let pilih = pilihan.trim().chars().next();
+
+    let result = match pilih {
+        Some('*') => kali(user_input1, user_input2),
+        _ => {
+            println!("inputan salah");
+            return;
+        }
+    };
+
+    println!("hasil: {}", result);
+}
+
+fn kali(a: i32, b: i32) -> i32 {
+    let result = a * b;
+    return result;
 }
